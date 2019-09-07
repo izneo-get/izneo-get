1dc0f57e5af671bcd31aea533e4b7182f48aa3a0
izneo-get <izneo-get.github.com@e-courrier.eu>
2019-09-07 21:48:45 +0200

    0.5 Compression en webp

--- izneo_get.py
+++ izneo_get.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.04"
+__version__ = "0.05"
 """
+Source : https://github.com/izneo-get/izneo-get
+
 Ce script permet de récupérer une BD présente sur https://www.izneo.com/fr/ dans la limite des capacités de notre compte existant.
 
-usage: python izneo_get.py [-h] [--session-id SESSION_ID] [--cfduid CFDUID]
+usage: izneo_get.py [-h] [--session-id SESSION_ID] [--cfduid CFDUID]
                     [--output-folder OUTPUT_FOLDER]
-                    [--output-format {both,jpg,cbz}] [--config CONFIG]
+                    [--output-format {jpg,both,cbz}] [--config CONFIG]
                     [--from-page FROM_PAGE] [--limit LIMIT] [--pause PAUSE]
                     [--full-only] [--continue] [--user-agent USER_AGENT]
+                    [--webp WEBP]
                     url
 
 Script pour sauvegarder une BD Izneo.
@@ -24,7 +27,7 @@ optional arguments:
                         L'identifiant cfduid
   --output-folder OUTPUT_FOLDER, -o OUTPUT_FOLDER
                         Répertoire racine de téléchargement
-  --output-format {both,jpg,cbz}, -f {both,jpg,cbz}
+  --output-format {jpg,both,cbz}, -f {jpg,both,cbz}
                         Répertoire racine de téléchargement
   --config CONFIG       Fichier de configuration
   --from-page FROM_PAGE
@@ -37,12 +40,15 @@ optional arguments:
   --continue            Pour reprendre là où on en était
   --user-agent USER_AGENT
                         User agent à utiliser
+  --webp WEBP           Conversion en webp avec une certaine qualité (exemple : --webp 75)
 
 CFDUID est la valeur de "cfduid" dans le cookie.
 SESSION_ID est la valeur de "c03aab1711dbd2a02ea11200dde3e3d1" dans le cookie.
 Ces valeurs peuvent être stockées dans le fichier de configuration "izneo_get.cfg".
 """
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 import re
 import os
 import sys 
@@ -52,6 +58,8 @@ import configparser
 import shutil
 import time
 from bs4 import BeautifulSoup
+from PIL import Image
+
 
 def strip_tags(html):
     """Permet de supprimer tous les tags HTML d'une chaine de caractère.
@@ -87,6 +95,27 @@ def clean_name(name):
     name = re.sub(r"\s+", " ", name)
     return name
 
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=1,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    """Permet de gérer les cas simples de problèmes de connexions.
+    """
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 if __name__ == "__main__":
     cfduid = ""
@@ -134,6 +163,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--user-agent", type=str, default=None, help="User agent à utiliser"
     )
+    parser.add_argument(
+        "--webp", type=int, default=None, help="Conversion en webp avec une certaine qualité (exemple : --webp 75)"
+    )
     args = parser.parse_args()
 
  
@@ -160,15 +192,16 @@ if __name__ == "__main__":
     cfduid = get_param_or_default(config, "cfduid", "", args.cfduid)
     session_id = get_param_or_default(config, "session_id", "", args.session_id)
     user_agent = get_param_or_default(config, "user_agent", "", args.user_agent)
+    pause_sec =  get_param_or_default(config, "pause", "", args.pause)
     output_folder = get_param_or_default(config, "output_folder", os.path.dirname(os.path.abspath(sys.argv[0])) + "/DOWNLOADS", args.output_folder)
     if not os.path.exists(output_folder): os.mkdir(output_folder)
     url = args.url
     output_format = args.output_format
     nb_page_limit = args.limit
     from_page = args.from_page
-    pause_sec = args.pause
     full_only = args.full_only
     continue_from_existing = args.continue_from_existing
+    webp = args.webp
 
     # Création d'une session et création du cookie.
     s = requests.Session()
@@ -197,7 +230,8 @@ if __name__ == "__main__":
     for url in url_list:
         print("URL: " + url)
         # On récupère les informations de la BD à récupérer.
-        r = s.get(url, cookies=s.cookies, allow_redirects=True)
+        # r = s.get(url, cookies=s.cookies, allow_redirects=True)
+        r = requests_retry_session(session=s).get(url, cookies=s.cookies, allow_redirects=True)
         html_one_line = r.text.replace("\n", "").replace("\r", "")
         
         soup = BeautifulSoup(html_one_line, features="html.parser")
@@ -273,9 +307,12 @@ if __name__ == "__main__":
             mid_path += "/"
 
         print("Téléchargement de \"" + clean_name(title + serie + author) + "\"")
-        # print(str(nb_pages) + " pages attendues")
         print("{nb_pages} pages attendues".format(nb_pages=nb_pages))
         save_path = output_folder + "/" + mid_path + clean_name(title + serie + author)
+        # Si l'archive existe déjà, on ne télécharge pas cette BD.
+        if continue_from_existing and os.path.exists(save_path + ".cbz"):
+            print(save_path + ".cbz existe déjà, on passe")
+            continue
         if not os.path.exists(save_path): os.mkdir(save_path)
         print("Destination : " + save_path)
 
@@ -299,10 +336,19 @@ if __name__ == "__main__":
 
             page_txt = ("000000000" + str(page_num))[-nb_digits:]
             store_path = save_path + "/" + title + " - " + page_txt + ".jpg"
+            store_path_webp = save_path + "/" + title + " - " + page_txt + ".webp"
+
 
             url = "https://reader.izneo.com/read/" + str(isbn) +  "/" + str(page_num) + "?quality=HD"
             # Si la page existe déjà sur le disque, on passe.
-            if continue_from_existing and os.path.exists(store_path):
+            if (
+                    continue_from_existing and
+                    (
+                        (not webp and os.path.exists(store_path) and os.path.getsize(store_path)) 
+                        or 
+                        (webp and os.path.exists(store_path_webp) and os.path.getsize(store_path_webp))
+                    )
+                ):
                 progress_bar += "x"
                 progress_message = "\r" + "[page " + str(page_num) + " / ~" + str(nb_pages) + "] " + progress_bar + " "
                 # print("x", end="")
@@ -310,7 +356,9 @@ if __name__ == "__main__":
                 sys.stdout.flush()
                 continue
 
-            r = s.get(url, cookies=s.cookies, allow_redirects=True, params=params, headers=headers)
+            # r = s.get(url, cookies=s.cookies, allow_redirects=True, params=params, headers=headers)
+            r = requests_retry_session(session=s).get(url, cookies=s.cookies, allow_redirects=True, params=params, headers=headers)
+
             if r.status_code == 404:
                 if page < nb_pages:
                     print("[WARNING] On a récupéré " + str(page + 1) + " pages (" + str(nb_pages) + " annoncées par l'éditeur)")
@@ -321,6 +369,11 @@ if __name__ == "__main__":
                 print("[WARNING] Page " + str(page_num) + " inaccessible")
                 break
             file = open(store_path, "wb").write(r.content)
+            # Si demandé, on converti en webp.
+            if webp:
+                im = Image.open(store_path)
+                im.save(store_path_webp, 'webp', quality=webp)
+                os.remove(store_path)
             progress_bar += "."
             progress_message = "\r" + "[page " + str(page_num) + " / ~" + str(nb_pages) + "] " + progress_bar + " "
             # print(".", end="")
