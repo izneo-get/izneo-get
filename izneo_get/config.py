5da51101ec11e51a6a32df28ad3a0954f72949b7
izneo-get <izneo-get@noreply.github.com>
2023-05-03 23:32:05 +0200

    Change args handling

--- izneo_get/config.py
+++ izneo_get/config.py
@@ -1,17 +1,47 @@
+import configparser
 from dataclasses import dataclass
 from enum import Enum
+from typing import Any, Dict, Optional
 
 
 class ImageFormat(Enum):
-    ORIGIN = 1
-    JPEG = 2
-    WEBP = 3
+    ORIGIN = "ORIGIN"
+    JPEG = "JPEG"
+    WEBP = "WEBP"
+
+    @staticmethod
+    def from_str(value: str) -> Optional["ImageFormat"]:
+        if not value:
+            return None
+        value = value.upper()
+        if value == "ORIGIN":
+            return ImageFormat.ORIGIN
+        elif value in {"JPEG", "JPG"}:
+            return ImageFormat.JPEG
+        elif value == "WEBP":
+            return ImageFormat.WEBP
+        else:
+            return None
 
 
 class OutputFormat(Enum):
-    IMAGES = 1
-    CBZ = 2
-    BOTH = 3
+    IMAGES = "IMAGES"
+    CBZ = "CBZ"
+    BOTH = "BOTH"
+
+    @staticmethod
+    def from_str(value: str) -> Optional["OutputFormat"]:
+        if not value:
+            return None
+        value = value.upper()
+        if value == "IMAGES":
+            return OutputFormat.IMAGES
+        elif value == "CBZ":
+            return OutputFormat.CBZ
+        elif value == "BOTH":
+            return OutputFormat.BOTH
+        else:
+            return None
 
 
 @dataclass
@@ -28,3 +58,15 @@ class Config:
     continue_from_existing: bool = False
     authentication_from_cache: bool = True
     cache_folder: str = ".cache"
+
+    def to_dict(self):
+        value: Dict[str, Any] = {key: str(val) for key, val in self.__dict__.items() if val is not None}
+        return value
+
+    def save_config(self, path: str) -> None:
+        config = configparser.RawConfigParser()
+        config_as_dict = self.to_dict()
+        del config_as_dict["cache_folder"]
+        config["DEFAULT"] = config_as_dict
+        with open(path, "w") as f:
+            config.write(f)
--- izneo_get/config_from_file.py
+++ izneo_get/config_from_file.py
@@ -6,7 +6,7 @@ from .config import Config, ImageFormat, OutputFormat
 from argparse import Namespace
 
 
-def get_config_from_file(config_file: str, args_config: Namespace) -> Config:
+def get_config_from_file(config_file: str, args_config: Config) -> Config:
     default_config = Config()
     # Lecture de la config.
     config = configparser.RawConfigParser()
@@ -34,28 +34,24 @@ def get_config_from_file(config_file: str, args_config: Namespace) -> Config:
     output_filename = get_param_or_default(
         config, "output_filename", default_config.output_filename, args_config.output_filename
     )
-    match_image_format = {
-        "ORIGIN": ImageFormat.ORIGIN,
-        "JPEG": ImageFormat.JPEG,
-        "WEBP": ImageFormat.WEBP,
-    }
     image_format = get_param_or_default(
-        config, "image_format", str(default_config.image_format).split(".")[-1], args_config.image_format
+        config,
+        "image_format",
+        str(default_config.image_format).split(".")[-1],
+        args_config.image_format.value if args_config.image_format else None,
     )
-    image_format = match_image_format[image_format.upper()]
+    image_format = ImageFormat.from_str(image_format)
     image_quality = int(
         get_param_or_default(config, "image_quality", default_config.image_quality, args_config.image_quality)
     )
-    match_output_format = {
-        "CBZ": OutputFormat.CBZ,
-        "IMAGES": OutputFormat.IMAGES,
-        "BOTH": OutputFormat.BOTH,
-    }
     output_format = get_param_or_default(
-        config, "output_format", str(default_config.output_format).split(".")[-1], args_config.output_format
+        config,
+        "output_format",
+        str(default_config.output_format).split(".")[-1],
+        args_config.output_format.value if args_config.output_format else None,
     )
-    output_format = match_output_format[output_format.upper()]
-    pause_sec = int(get_param_or_default(config, "pause", default_config.pause_sec, args_config.pause))
+    output_format = OutputFormat.from_str(output_format)
+    pause_sec = int(get_param_or_default(config, "pause", default_config.pause_sec, args_config.pause_sec))
     user_agent = get_param_or_default(config, "user_agent", default_config.user_agent, args_config.user_agent)
     continue_from_existing = get_param_or_default(
         config, "continue_from_existing", default_config.continue_from_existing, args_config.continue_from_existing
